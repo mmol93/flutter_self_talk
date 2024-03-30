dbd1b995159c3d9b018a8672ccc446ca4da27aca
ljws93 <ljws93@naver.com>
2024-03-30 16:54:36 +0900

    1. 메인 화면의 Appbar 색상 설정 2. 사용할 카톡 기본 색상 정의 3. 기본 폰트 적용

---
+++ lib/colors/default.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const defaultBackground = Color.fromRGBO(178, 199, 217, 1);
+const defaultAppbarColor = Color.fromRGBO(103, 123, 172, 1);
+const defaultYellow = Color.fromRGBO(253, 216, 1, 1);
+const defaultProfileBackground = Color.fromRGBO(127, 189, 174, 1);
+const defaultGrayBackground = Color.fromRGBO(211, 213, 208, 1);
