799a53f420bcd1ad6def64f26295a91823d730b5
ljws93 <ljws93@naver.com>
2024-04-25 12:06:25 +0900

    1. 프로필 사진 양식 최대한 비슷하게 만듬

--- lib/widgets/common/profile_picture.dart
+++ lib/widgets/common/profile_picture.dart
@@ -27,6 +27,7 @@ class ProfilePicture extends StatelessWidget {
               File(picturePath),
               height: imgHeight,
               width: imgWidth,
+              fit: BoxFit.cover,
               errorBuilder: (context, exception, stackTrace) {
                 return Image.asset(Strings.defaultProfileImgPath);
               },
@@ -39,30 +40,45 @@ class ProfilePicture extends StatelessWidget {
 }
 
 class MyClipper extends CustomClipper<Path> {
+  final double firstLine = 17;
+  final double secondLine = 26;
+
   @override
   Path getClip(Size size) {
     Path path = Path()
       ..moveTo(0, 21)
       ..arcTo(
           Rect.fromCenter(
-              center: Offset(20.5, 20.5),
-              width: size.width,
-              height: size.height),
+              center: Offset(firstLine, firstLine),
+              width: firstLine * 2,
+              height: firstLine * 2),
           1 * pi,
           0.5 * pi,
           false)
-      ..lineTo(size.width, 0)
-      ..arcTo(Rect.fromCircle(center: Offset(23.5, 20.5), radius: 21.5),
-          1.5 * pi, 0.5 * pi, false)
-      ..lineTo(size.width, size.width / 2)
+      ..lineTo(size.width * 0.67, 0)
       ..arcTo(
+        Rect.fromCenter(
+            center: Offset(secondLine, firstLine+1.4),
+            width: firstLine * 2 + 3,
+            height: firstLine * 2 + 3),
+        1.5 * pi,
+        0.5 * pi,
+        false,
+      )
+      ..lineTo(size.width, size.width * 0.67)
+      ..arcTo(
+          // 14.7, 14.7, 44, 44
           Rect.fromLTRB(size.width / 3, size.width / 3, size.width, size.width),
           2 * pi,
           0.5 * pi,
           false)
-      ..lineTo(size.width / 2, size.width)
-      ..arcTo(Rect.fromLTWH(0, size.width / 2, size.width / 2, size.width / 2),
-          0.5 * pi, 0.5 * pi, false)
+      ..lineTo(size.width * 0.67, size.width)
+      ..arcTo(
+        Rect.fromLTRB(0, size.width / 3-1, size.width-15, size.width),
+        0.5 * pi,
+        0.5 * pi,
+        false,
+      )
       ..close();
     return path;
   }
