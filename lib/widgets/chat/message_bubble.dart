b7563ffd724ab40a8e0c132d66d1208362e86387
ljws93 <ljws93@naver.com>
2024-06-09 17:56:18 +0900

    change git ignore list

---
+++ lib/widgets/chat/message_bubble.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:self_talk/models/chat.dart';
+
+class MessageBubble extends StatelessWidget {
+  const MessageBubble({super.key, required this.message});
+
+  final Message message;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
