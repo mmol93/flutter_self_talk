8c79dbf28c35cec1c3eb46ead611c7f017efd37a
ljws93 <ljws93@naver.com>
2024-06-25 22:37:30 +0900

    1. 채팅방 TextInput 디자인 완성

--- lib/screens/chat/chat_screen.dart
+++ lib/screens/chat/chat_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:self_talk/colors/default_color.dart';
 import 'package:self_talk/models/chat.dart';
 import 'package:self_talk/models/friend.dart';
@@ -21,6 +22,7 @@ class ChatScreen extends ConsumerStatefulWidget {
 class _ChatScreenState extends ConsumerState<ChatScreen> {
   late final String? currentChatRoomId;
   Friend? currentSelectedFriend;
+  final _messageInputProvider = StateProvider<String>((ref) => '');
 
   @override
   void initState() {
@@ -30,7 +32,8 @@ class _ChatScreenState extends ConsumerState<ChatScreen> {
   }
 
   /// 메시지 클릭 시 나오는 옵션 및 기능을 dialog로 표시
-  void _showMessageOptions(ChatViewModel viewModel, Chat targetChatData, int index) {
+  void _showMessageOptions(
+      ChatViewModel viewModel, Chat targetChatData, int index) {
     final Message message = targetChatData.messageList![index];
 
     showListDialog(
@@ -60,7 +63,8 @@ class _ChatScreenState extends ConsumerState<ChatScreen> {
     showListDialog(context: context, listItemModel: [
       for (var friend in targetChatData.chatMember)
         ListItemModel(
-          itemTitle: friend.name == me.name ? "(자신)${friend.name}" : friend.name,
+          itemTitle:
+              friend.name == me.name ? "(자신)${friend.name}" : friend.name,
           clickEvent: () {
             currentSelectedFriend = friend;
             setState(() {});
@@ -121,6 +125,7 @@ class _ChatScreenState extends ConsumerState<ChatScreen> {
       ),
     );
   }
+
   final inputTextController = TextEditingController();
 
   @override
@@ -178,66 +183,85 @@ class _ChatScreenState extends ConsumerState<ChatScreen> {
                 },
                 child: Text(
                   currentSelectedFriend != null
-                      ? me == currentSelectedFriend ? "(자신)${currentSelectedFriend!.name}" : currentSelectedFriend!.name
+                      ? me == currentSelectedFriend
+                          ? "(자신)${currentSelectedFriend!.name}"
+                          : currentSelectedFriend!.name
                       : "선택된 친구 없음",
                   style: const TextStyle(fontWeight: FontWeight.bold),
                 ),
               )
             ],
           ),
-          // TODO: 아직 미완성
+          // TODO: 기능 부분 아직 미완성 & 디자인 부분은 거의 완성한듯?
           ConstrainedBox(
             constraints: const BoxConstraints(maxHeight: 120),
-            child: Container(
-              color: Colors.white,
-              child: Row(
-                children: [
-                  IconButton(
-                    // TODO: 미디어 파일 업로드 기능 필요
-                    onPressed: () {},
-                    icon: const Icon(Icons.add),
-                    color: Colors.grey,
-                  ),
-                  Expanded(
-                    child: TextField(
-                      onTapOutside: (pointerDownEvent) {
-                        FocusManager.instance.primaryFocus?.unfocus();
-                      },
-                      controller: inputTextController,
-                      maxLines: null,
-                      decoration: const InputDecoration(
-                        border: InputBorder.none, // 밑줄 제거
-                      ),
-                      onChanged: (text) {
-                        // 뭔가를 입력하면 보내기 버튼으로 바꾸기
-                        if (text.isNotEmpty) {
-                          inputTextController.text = text;
-                          // TODO: 메시지 보내기 기능 넣기 & 보내기 버튼 디자인으로 바꾸는 기능
-                        }
-                      },
-                    ),
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: Image.asset(
-                      'assets/images/kid.png',
-                      width: 33,
-                      height: 33,
-                      color: Colors.grey,
-                    ),
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Text(
-                      '＃',
-                      style: TextStyle(
-                          fontSize: 22,
+            child: Consumer(
+              builder: (context, ref, child) {
+                final messageText = ref.watch(_messageInputProvider);
+                return Container(
+                  color: Colors.white,
+                  // IntrinsicHeight를 사용하면 Row 위젯의 자식의 height도 같이 커지게 할 수 있다.
+                  child: IntrinsicHeight(
+                    child: Row(
+                        // IntrinsicHeight을 사용함과 동시에 CrossAxisAlignment.stretch도 적용해야한다.
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        IconButton(
+                          // TODO: 미디어 파일 업로드 기능 필요
+                          onPressed: () {},
+                          icon: const Icon(Icons.add),
                           color: Colors.grey,
-                          fontWeight: FontWeight.w500),
+                        ),
+                        Expanded(
+                          child: TextField(
+                            onTapOutside: (pointerDownEvent) {
+                              FocusManager.instance.primaryFocus?.unfocus();
+                            },
+                            controller: inputTextController,
+                            maxLines: null,
+                            decoration: const InputDecoration(
+                              border: InputBorder.none, // 밑줄 제거
+                            ),
+                            onChanged: (text) {
+                              // 뭔가를 입력하면 보내기 버튼으로 바꾸기
+                              if (text.isNotEmpty) {
+                                inputTextController.text = text;
+                                // TODO: 메시지 보내기 기능 넣기 추가 해야함
+                              }
+                              ref.read(_messageInputProvider.notifier).state = text;
+                            },
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: Image.asset(
+                            'assets/images/kid.png',
+                            width: 34,
+                            height: 34,
+                            color: Colors.grey,
+                          ),
+                        ),
+                        if (messageText.isEmpty) Container(
+                          padding: const EdgeInsets.all(13),
+                          child: SvgPicture.asset(
+                              'assets/images/sharp.svg',
+                              width: 20,
+                              colorFilter: const ColorFilter.mode(Colors.grey, BlendMode.srcIn),
+                          ),
+                        ) else GestureDetector(
+                          onTap: () {},
+                          child: Container(
+                            padding: const EdgeInsets.all(13),
+                            alignment: Alignment.bottomCenter,
+                            decoration: const BoxDecoration(color: defaultYellow),
+                            child: Icon(size: 20, Icons.send, color: Colors.black),
+                          ),
+                        ),
+                      ],
                     ),
-                  )
-                ],
-              ),
+                  ),
+                );
+              },
             ),
           ),
           TextButton(
