797b83da3129925e12f5cfc5092294c2995d7742
ljws93 <ljws93@naver.com>
2025-01-11 14:03:06 +0900

    1. (버그) @를 이용한 친구 태깅 기능이 제대로 동작하지 않는 문제

--- lib/widgets/chat/message_mention_text_span.dart
+++ lib/widgets/chat/message_mention_text_span.dart
@@ -1,24 +1,53 @@
 import 'package:flutter/material.dart';
 
+/// @가 있을 경우 친구 태그 기능을 넣어주는 TextSpan
 List<TextSpan> friendMentionColoredTextSpans(
     String textMessage,
     bool isDeleted,
     ) {
   List<TextSpan> spans = [];
-  List<String> parts = textMessage.split(' ');
+  List<String> lines = textMessage.split('\n');
 
-  for (String part in parts) {
-    if (part.contains('@')) {
-      // @가 포함된 부분은 파란색으로
-      spans.add(TextSpan(
-          text: '$part ',
-          style: const TextStyle(color: Colors.blue, fontWeight: FontWeight.w500)));
-    } else {
-      // 나머지 부분은 기본 검은색
+  for (int i = 0; i < lines.length; i++) {
+    String line = lines[i];
+    List<String> parts = line.split(' ');
+
+    for (int j = 0; j < parts.length; j++) {
+      String part = parts[j];
+      if (part.isNotEmpty) {  // 빈 문자열 건너뛰기
+        if (part.startsWith('@')) {
+          spans.add(
+              TextSpan(
+                  text: '$part${j < parts.length - 1 ? ' ' : ''}',
+                  style: const TextStyle(
+                      color: Colors.blue,
+                      fontWeight: FontWeight.w500
+                  )
+              )
+          );
+        } else {
+          spans.add(
+            TextSpan(
+              text: '$part${j < parts.length - 1 ? ' ' : ''}',
+              style: TextStyle(
+                  color: isDeleted ? Colors.grey : Colors.black,
+                  fontWeight: FontWeight.w300
+              ),
+            ),
+          );
+        }
+      }
+    }
+
+    // 마지막 줄이 아니면 줄바꿈 추가
+    if (i < lines.length - 1) {
       spans.add(
         TextSpan(
-          text: '$part ',
-          style: TextStyle(color: isDeleted ? Colors.grey : Colors.black, fontWeight: FontWeight.w300),
+          text: '\n',
+          style: TextStyle(
+              color: isDeleted ? Colors.grey : Colors.black,
+              fontWeight: FontWeight.w300
+          ),
         ),
       );
     }
